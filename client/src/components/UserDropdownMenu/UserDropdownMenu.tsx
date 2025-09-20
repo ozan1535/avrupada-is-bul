a15f046a19aa924507a806c69464bc492c32bbfe
ozan1535 <ozanbilgic1535@gmail.com>
2025-09-20 09:55:52 +0300

    Add favorite link

--- client/src/components/UserDropdownMenu/UserDropdownMenu.tsx
+++ client/src/components/UserDropdownMenu/UserDropdownMenu.tsx
@@ -16,6 +16,7 @@ import {
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { syncUserProfile } from "@/lib/supabase/sync-user";
 import Image from "next/image";
+import Link from "next/link";
 
 async function UserDropdownMenu() {
   const { getUser } = getKindeServerSession();
@@ -37,6 +38,11 @@ async function UserDropdownMenu() {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 z-[101]" align="start">
         <DropdownMenuLabel>Hesabım</DropdownMenuLabel>
+        <DropdownMenuItem>
+          <Link href={"/favoriler"} className="text-subtle">
+            Favoriler
+          </Link>
+        </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           {user ? (
